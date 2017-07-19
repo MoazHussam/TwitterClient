aceb07545fecd5cb88247654fd68546b8d762545
Moaz Ahmed <MoazHussam>
2017-07-19 19:54:40 +0200

    Implement CoreData model

---
+++ TwitterClient/Tweet.swift
@@ -0,0 +1,14 @@
+//
+//  Tweet.swift
+//  TwitterClient
+//
+//  Created by Moaz Ahmed on 7/19/17.
+//  Copyright © 2017 Moaz Ahmed. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class Tweet: NSManagedObject {
+
+}
---
+++ TwitterClient/TwitterUser.swift
@@ -0,0 +1,15 @@
+//
+//  TwitterUser.swift
+//  TwitterClient
+//
+//  Created by Moaz Ahmed on 7/19/17.
+//  Copyright © 2017 Moaz Ahmed. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class TwitterUser: NSManagedObject {
+
+    
+}
